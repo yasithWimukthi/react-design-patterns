681d64becc1574acfa6d3e5d7bd58334e2e21c60
yasith <it19966922@my.sliit.lk>
2022-12-10 10:59:24 +0530

    container components

--- src/App.js
+++ src/App.js
@@ -62,6 +62,12 @@ const getServerData = url => async () => {
     return response.data;
 }
 
+const getLocalStorageData = key => () => {
+    return localStorage.getItem(key);
+}
+
+const Text = ({ message }) => <h1>{message}</h1>;
+
 function App() {
     return (
         // <>
@@ -112,6 +118,9 @@ function App() {
             </ResourceLoader>
             <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
                 <UserInfo />
+            </DataSource>
+            <DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
+                <Text />
             </DataSource>
         </>
 
