5b71a4bb520684ac79580000fc8edfb679690e67
yasith <it19966922@my.sliit.lk>
2022-12-10 09:48:22 +0530

    layout components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from './SplitScreen';
+
+const LeftHandComponent = () => {
+	return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
+}
+
+const RightHandComponent = () => {
+	return <p style={{ backgroundColor: 'red' }}>Right!</p>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<SplitScreen
+			left={LeftHandComponent}
+			right={RightHandComponent} />
+	);
 }
 
 export default App;
